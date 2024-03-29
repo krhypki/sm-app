168fffb610857af99d7ca2203de3308afc8b3ce0
Krzysztof Hypki <khypki@pc15.home>
2024-03-29 13:03:48 +0100

    Remove unnecesary sleep invoke

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,10 +1,8 @@
 'use server';
 
 import { getUsersPerPage } from '@/lib/db/user';
-import { sleep } from '@/lib/utils/sleep';
 
 export async function findPeople(page: number, query: string) {
-  await sleep(2000);
   const { totalPages, users } = await getUsersPerPage(page, query);
 
   return { totalPages, users };
